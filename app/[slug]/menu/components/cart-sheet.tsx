b19ef64a8e3935d38fd94f41946fa3b644b753db
erick <erickwelberth1@hotmail.com>
2025-02-24 21:16:33 -0300

    feat: Finalize order feature [drawer]

--- app/[slug]/menu/components/cart-sheet.tsx
+++ app/[slug]/menu/components/cart-sheet.tsx
@@ -1,43 +1,60 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
 import FormatValue from "@/helpers/format-value";
 
 import { CartContext } from "../context/cart";
 import CartProductItem from "./cart-product-item";
-import FinishOrderButton from "./finish-order-button";
+import FinishOrderDialog from "./finish-order-dialog";
 
 const CartSheet = () => {
-    const {isOpen, toggleCart, products, total} = useContext(CartContext)
+  const [finishOrderDialogIsOpen, setFinishOrderDialogIsOpen] = useState(false);
+  const { isOpen, toggleCart, products, total } = useContext(CartContext);
 
-    return (
-        <Sheet open={isOpen} onOpenChange={toggleCart}>
-          <SheetContent className="w-[88%]">
-            <SheetHeader>
-              <SheetTitle className="text-left">Sacola</SheetTitle>
-            </SheetHeader>
-            <div className="flex flex-col h-full py-5">
-              <div className="flex-auto">
-                {products.map((product) => (
-                <CartProductItem key={product.id} product={product}/>
-                 ))}
+  return (
+    <Sheet open={isOpen} onOpenChange={toggleCart}>
+      <SheetContent className="w-[88%]">
+        <SheetHeader>
+          <SheetTitle className="text-left">Sacola</SheetTitle>
+        </SheetHeader>
+        <div className="flex h-full flex-col py-5">
+          <div className="flex-auto">
+            {products.map((product) => (
+              <CartProductItem key={product.id} product={product} />
+            ))}
+          </div>
+
+          <Card className="mb-6">
+            <CardContent className="p-5">
+              <div className="flex justify-between">
+                <p className="text-sm text-muted-foreground">Total</p>
+                <p className="text-sm font-semibold">{FormatValue(total)}</p>
               </div>
+            </CardContent>
+          </Card>
 
-              <Card className="mb-6">
-                <CardContent className="p-5">
-                  <div className="flex justify-between">
-                    <p className="text-sm text-muted-foreground">Total</p>
-                    <p className="text-sm font-semibold">{FormatValue(total)}</p>
-                  </div>
-                </CardContent>
-              </Card>
+          <Button
+            className="w-full rounded-full"
+            onClick={() => setFinishOrderDialogIsOpen(true)}
+          >
+            Finalizar Pedido
+          </Button>
 
-              <FinishOrderButton />
-            </div>
-          </SheetContent>
-        </Sheet>
-    )
-}
+          <FinishOrderDialog
+            open={finishOrderDialogIsOpen}
+            onOpenChange={setFinishOrderDialogIsOpen}
+          />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default CartSheet;
---
+++ app/[slug]/menu/components/finish-order-dialog.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormProvider, useForm } from "react-hook-form";
+import { PatternFormat } from "react-number-format";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { isValidCpf } from "@/helpers/format-cpf";
+
+const FormSchema = z.object({
+  name: z.string().trim().min(2, {
+    message: "O nome é obrigatório.",
+  }),
+  cpf: z
+    .string()
+    .trim()
+    .min(1, {
+      message: "O CPF é obrigatório",
+    })
+    .refine((value) => isValidCpf(value), {
+      message: "CPF inválido.",
+    }),
+});
+
+type formSchema = z.infer<typeof FormSchema>;
+
+interface FinishOrderDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
+  const form = useForm<formSchema>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      cpf: "",
+    },
+    //clean form
+    shouldUnregister: true,
+  });
+
+  const onSubmit = (data: formSchema) => {
+    console.log(data);
+  };
+
+  return (
+    <Drawer open={open} onOpenChange={onOpenChange}>
+      <DrawerTrigger asChild></DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Finalizar Pedido</DrawerTitle>
+          <DrawerDescription>
+            Insira suas informações abaixo para finalizar o seu pedido.
+          </DrawerDescription>
+        </DrawerHeader>
+        <div className="p-5">
+          <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Seu nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu nome..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="cpf"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Seu CPF</FormLabel>
+                    <FormControl>
+                      <PatternFormat
+                        placeholder="Digite seu CPF..."
+                        format="###.###.###-##"
+                        customInput={Input}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <DrawerFooter>
+                <Button
+                  type="submit"
+                  variant="destructive"
+                  className="rounded-full"
+                >
+                  Finalizar
+                </Button>
+                <DrawerClose asChild>
+                  <Button variant="outline" className="w-full rounded-full">
+                    Cancelar
+                  </Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </form>
+          </FormProvider>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default FinishOrderDialog;
